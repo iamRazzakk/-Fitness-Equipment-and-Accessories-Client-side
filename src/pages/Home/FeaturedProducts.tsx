9be028a49c5f8a75a6cba0586d7c671faee1c899
iamRazzakk <rakibt23p@gmail.com>
2024-08-21 19:24:16 -0400

    update featured Product use swiperjs for caruse

--- src/pages/Home/FeaturedProducts.tsx
+++ src/pages/Home/FeaturedProducts.tsx
@@ -1,26 +1,133 @@
 import { Button } from "@/components/ui/button";
 import exerciseBallImg from "../../assets/treadmill.png";
 import "../../ExternalCss/Card.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+// import required modules
+import { Scrollbar } from "swiper/modules";
 const FeaturedProducts = () => {
   return (
     <div className="lg:mt-32 md:mt-12 mt-8">
       <h1 className="text-3xl font-bold">Featured Products</h1>
       {/* Product card */}
-      <div className="lg:mt-8 max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-        <img
-          className="w-full h-48 object-cover img"
-          src={exerciseBallImg}
-          alt={exerciseBallImg}
-        />
-        <div className="p-6">
-          <h2 className="text-xl font-bold text-gray-800">title</h2>
-          <p className="mt-2 text-gray-600">description</p>
-          <div className="mt-4 flex items-start justify-between">
-            <Button>View Details</Button>
-            <Button variant="secondary">Add to cart</Button>
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={15}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Scrollbar]}
+        className="mySwiper lg:mt-8 md:mt-6 mt-4"
+      >
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
           </div>
-        </div>
-      </div>
+        </SwiperSlide>
+
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
+          <img
+            className="w-full h-48 object-cover img"
+            src={exerciseBallImg}
+            alt="Exercise Ball"
+          />
+          <div className="p-6">
+            <h2 className="text-xl font-bold text-gray-800">Title</h2>
+            <p className="mt-2 text-gray-600">Description</p>
+            <div className="mt-4 flex items-start justify-between">
+              <Button>View Details</Button>
+              <Button variant="secondary">Add to Cart</Button>
+            </div>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
