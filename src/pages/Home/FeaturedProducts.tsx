613e251b4796282aae2d10f6887ce145fd381815
iamRazzakk <rakibt23p@gmail.com>
2024-08-23 23:44:29 -0400

    featured products section do dynamic

--- src/pages/Home/FeaturedProducts.tsx
+++ src/pages/Home/FeaturedProducts.tsx
@@ -1,14 +1,22 @@
 import { Button } from "@/components/ui/button";
-import exerciseBallImg from "../../assets/treadmill.png";
 import "../../ExternalCss/Card.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
-// import required modules
+
+// Import required modules
 import { Scrollbar } from "swiper/modules";
+import { useGetProductsQuery } from "@/redux/api/baseApi";
+
 const FeaturedProducts = () => {
+  const { data: Products, isLoading } = useGetProductsQuery([]);
+
+  if (isLoading) {
+    return <h1>Loading</h1>; // Make sure to return the JSX here
+  }
+
   return (
     <div className="lg:mt-32 md:mt-12 mt-8">
       <h1 className="text-3xl font-bold">Featured Products</h1>
@@ -36,109 +44,30 @@ const FeaturedProducts = () => {
         modules={[Scrollbar]}
         className="mySwiper lg:mt-8 md:mt-6 mt-4"
       >
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
+        {Products?.data?.slice(0, 8).map((product) => (
+          <SwiperSlide
+            key={product._id}
+            className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container"
+          >
+            <div className="p-3 bg-gray-400">
+              <img
+                className="w-full h-48 object-cover img"
+                src={product.images}
+                alt={product.product}
+              />
             </div>
-          </div>
-        </SwiperSlide>
-
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
+            <div className="p-6">
+              <h2 className="text-xl font-bold text-gray-800">
+                {product.product}
+              </h2>
+              <p className="mt-2 text-gray-600">{product.description}</p>
+              <div className="mt-4 flex items-start justify-between">
+                <Button>View Details</Button>
+                <Button variant="secondary">Add to Cart</Button>
+              </div>
             </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
-            </div>
-          </div>
-        </SwiperSlide>
-        <SwiperSlide className="max-w-sm w-full bg-white rounded-lg shadow-lg overflow-hidden img-container">
-          <div className="p-3 bg-gray-400">
-            <img
-              className="w-full h-48 object-cover img"
-              src={exerciseBallImg}
-              alt="Exercise Ball"
-            />
-          </div>
-          <div className="p-6">
-            <h2 className="text-xl font-bold text-gray-800">Title</h2>
-            <p className="mt-2 text-gray-600">Description</p>
-            <div className="mt-4 flex items-start justify-between">
-              <Button>View Details</Button>
-              <Button variant="secondary">Add to Cart</Button>
-            </div>
-          </div>
-        </SwiperSlide>
+          </SwiperSlide>
+        ))}
       </Swiper>
     </div>
   );
--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 export const baseApi = createApi({
     reducerPath: 'baseApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/api' }),
     endpoints: (builder) => ({
         getProducts: builder.query({
             query: () => ({
