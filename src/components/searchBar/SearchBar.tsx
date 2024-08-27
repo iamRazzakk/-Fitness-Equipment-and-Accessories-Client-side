784adb350ecffc525082a7d73ce97351db9e8fc3
iamRazzakk <rakibt23p@gmail.com>
2024-08-26 23:20:03 -0400

    implement debaounce using npm pacg

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -3,25 +3,30 @@ import { setSearchState } from "@/redux/features/searchSlice";
 import { Input } from "@/components/ui/input";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
+import React from "react";
+import useDebounce from "@/hook/useDebounce";
 
 const SearchBar = () => {
   const dispatch = useAppDispatch();
   const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+  const [inputValue, setInputValue] = React.useState(searchTerm);
+
+  // Debounce the search term
+  const debouncedSearchTerm = useDebounce(inputValue, 500);
+  React.useEffect(() => {
+    dispatch(setSearchState(debouncedSearchTerm));
+  }, [debouncedSearchTerm, dispatch]);
 
   const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    dispatch(setSearchState(e.target.value));
+    setInputValue(e.target.value);
   };
-
-  //   const handleClearSearch = () => {
-  //     dispatch(clearSearchState());
-  //   };
 
   return (
     <div className="flex items-center gap-2">
       <Input
         className="text-black lg:w-1/3 mx-auto"
         type="text"
-        value={searchTerm}
+        value={inputValue}
         onChange={handleSearchChange}
         placeholder="Search products..."
       />
